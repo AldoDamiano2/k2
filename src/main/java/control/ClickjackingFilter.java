fdfc7b57cb7dff236496db941235ffc720956f8a
AldoDamiano <a.damiano26@studenti.unisa.it>
2024-06-02 19:43:25 +0200

    id-5 resolved

--- src/main/java/control/ClickjackingFilter.java
+++ src/main/java/control/ClickjackingFilter.java
@@ -6,9 +6,12 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@WebFilter("/*")
 public class ClickjackingFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
